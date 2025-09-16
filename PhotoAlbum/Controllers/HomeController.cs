ac22a26ee3ba9e1f32172f0328449c630637b47c
Michael Trumbull <michael.trumbull@nscc.ca>
2025-09-16 11:29:16 -0300

    Added a comment for home controller.

--- PhotoAlbum/Controllers/HomeController.cs
+++ PhotoAlbum/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using PhotoAlbum.Models;
 
 namespace PhotoAlbum.Controllers
 {
+    // The Home Controller class
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
