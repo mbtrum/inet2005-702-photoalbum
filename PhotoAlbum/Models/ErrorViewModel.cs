91ddd1d0c80244f164d7414b57b427876157223b
Michael Trumbull <michael.trumbull@nscc.ca>
2025-09-16 11:24:39 -0300

    Add project files.

---
+++ PhotoAlbum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhotoAlbum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
