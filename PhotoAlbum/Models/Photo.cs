d37e76c793a11ac6f28b851d73b736b982240c8b
Michael Trumbull <michael.trumbull@nscc.ca>
2025-09-17 10:11:49 -0300

    Added Photo model to project.

---
+++ PhotoAlbum/Models/Photo.cs
@@ -0,0 +1,17 @@
+﻿namespace PhotoAlbum.Models
+{
+    public class Photo
+    {
+        // Primary key
+        public int PhotoId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Filename { get; set; } = string.Empty;
+
+        public DateTime CreateDate { get; set; }
+    }
+
+}
